7d7506e5eefac265ef3537d6bceeb0cb94c63408
Andrey Timoshpolsky <shpolsky@yandex-team.ru>
2014-11-14 02:20:16 +0300

    Problems

---
+++ problems/src/main/java/com/andreytim/jafar/problems/sorting/P94_SortAnagrams.java
@@ -0,0 +1,45 @@
+package com.andreytim.jafar.problems.sorting;
+
+import java.util.*;
+
+/**
+ * Write a method to sort an array of strings so that all the anagrams are next to each other.
+ * CtCI, 11.2
+ *
+ * Created by shpolsky on 14.11.14.
+ */
+public class P94_SortAnagrams {
+
+    public void sortAnagrams(String[] arr) {
+        if (arr.length < 3) return;
+        Map<String, List<String>> buckets = new HashMap<>();
+        for (String s : arr) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!buckets.containsKey(key)) {
+                buckets.put(key, new ArrayList<String>());
+            }
+            buckets.get(key).add(s);
+        }
+        int i = 0;
+        for (List<String> anagrams : buckets.values()) {
+            for (String s : anagrams) {
+                arr[i++] = s;
+            }
+        }
+    }
+
+    public static void test(String[] arr) {
+        System.out.println("Unsorted: " + Arrays.toString(arr));
+        new P94_SortAnagrams().sortAnagrams(arr);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        test(new String[]{ "aaa", "bbb", "aaa", "ccc", "bbb" });
+        test(new String[]{ "abc", "bbb", "cba", "ccc", "bac" });
+        test(new String[]{ "abcasdga", "bbbsadgas" });
+    }
+}
